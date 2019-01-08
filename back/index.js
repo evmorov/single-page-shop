a39faf0b9cfa3c7966c3f770c381a2283d7429f2
Evgeny Morozov <evmorov@gmail.com>
2019-01-08 22:40:59 +0300

    Remove warning

--- back/index.js
+++ back/index.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose');
 const Product = require('./models/product');
 
 const App = express();
-const connect = mongoose.connect('mongodb://localhost:27017/express-react-shop');
+const connect = mongoose.connect('mongodb://localhost:27017/express-react-shop',
+                                 { useNewUrlParser: true });
 
 App.get('/products', async (req, res) => {
   const products = await Product.find();
