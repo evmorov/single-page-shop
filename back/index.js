d9294a5cf2c4e989f9b20090d999ad383b08826a
Evgeny Morozov <evmorov@gmail.com>
2019-01-08 23:00:53 +0300

    Add eslint and style fixes

--- back/index.js
+++ back/index.js
@@ -4,8 +4,10 @@ const mongoose = require('mongoose');
 const Product = require('./models/product');
 
 const App = express();
-const connect = mongoose.connect('mongodb://localhost:27017/express-react-shop',
-                                 { useNewUrlParser: true });
+mongoose.connect(
+  'mongodb://localhost:27017/express-react-shop',
+  { useNewUrlParser: true }
+);
 
 App.get('/products', async (req, res) => {
   const products = await Product.find();
