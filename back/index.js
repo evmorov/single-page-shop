3c86b2a5622375cddc560480296b0158a80a661b
Evgeny Morozov <evmorov@gmail.com>
2019-01-11 23:32:07 +0300

    Some refactor and add show endpoint

--- back/index.js
+++ back/index.js
@@ -3,15 +3,22 @@ const mongoose = require('mongoose');
 
 const Product = require('./models/product');
 
-const App = express();
+const app = express();
+const port = 5001;
+
 mongoose.connect(
   'mongodb://localhost:27017/express-react-shop',
   { useNewUrlParser: true }
 );
 
-App.get('/products', async (req, res) => {
+app.get('/products', async (req, res) => {
   const products = await Product.find();
   res.json(products);
 });
 
-App.listen(5001, () => console.log('The server has started on port 5001'));
+app.get('/products/:id', async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  res.json(product);
+});
+
+app.listen(port, () => console.log(`The server has started on port ${port}`));
