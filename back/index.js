2b87bbaa6401a787a1b7827e7a9d8c0f9bbe60a8
Evgeny Morozov <evmorov@gmail.com>
2019-01-12 00:35:53 +0300

    Add create endpoint

--- back/index.js
+++ back/index.js
@@ -1,24 +1,36 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const Product = require('./models/product');
 
 const app = express();
 const port = 5001;
 
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
 mongoose.connect(
   'mongodb://localhost:27017/express-react-shop',
   { useNewUrlParser: true }
 );
 
-app.get('/products', async (req, res) => {
-  const products = await Product.find();
-  res.json(products);
-});
+app.route('/products')
+  .get(async (req, res) => {
+    const products = await Product.find();
+    res.json(products);
+  })
+  .post(async (req, res) => {
+    const product = await Product.create(req.body.product);
+    res.json(product);
+  });
 
-app.get('/products/:id', async (req, res) => {
-  const product = await Product.findById(req.params.id);
-  res.json(product);
-});
+app.route('/products/:id')
+  .get(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    res.json(product);
+  });
 
 app.listen(port, () => console.log(`The server has started on port ${port}`));
