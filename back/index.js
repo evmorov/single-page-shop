d35e32c060f5221d4b97107fd8b0758cf18c96c0
Evgeny Morozov <evmorov@gmail.com>
2019-01-15 23:11:04 +0300

    Tweak

--- back/index.js
+++ back/index.js
@@ -8,9 +8,7 @@ const Product = require('./models/product');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
