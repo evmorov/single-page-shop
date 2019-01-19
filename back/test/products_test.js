56ed2949365216da5239a7f6b54930d2b8fe3e20
Evgeny Morozov <evmorov@gmail.com>
2019-01-19 18:33:37 +0300

    Improve index tests

--- back/test/products_test.js
+++ back/test/products_test.js
@@ -2,18 +2,44 @@ const chai = require('chai');
 const request = require('supertest');
 
 const app = require('../index');
+const Product = require('../models/product');
 
 const { expect } = chai;
 
 describe('Products', function() {
-  describe('#GET / products', function() {
-    it('returns empty array when no products', function() {
-      request(app).get('/products')
-        .end((err, res) => {
-          expect(res.statusCode).to.equal(200);
-          expect(res.body).to.be.an('array');
-          expect(res.body).to.be.empty;
-        });
+  describe('#GET /products', function() {
+    describe('no products', function() {
+      it('returns empty array', function(done) {
+        request(app).get('/products')
+          .end((err, res) => {
+            expect(res.statusCode).to.equal(200);
+            expect(res.body).to.be.an('array');
+            expect(res.body).to.be.empty;
+            done();
+          });
+      });
+    });
+
+    describe('products exist', function() {
+      before(async function() {
+        await Product.create({ title: 'aaa', description: 'bbb', price: 1, images: [] });
+        await Product.create({ title: 'bbb', description: 'ccc', price: 2, images: [] });
+        await Product.create({ title: 'ddd', description: 'eee', price: 3, images: [] });
+      });
+
+      it('returns products', function(done) {
+        request(app).get('/products')
+          .end((err, res) => {
+            expect(res.statusCode).to.equal(200);
+            expect(res.body).to.be.an('array');
+            expect(res.body).to.have.lengthOf(3);
+            const keys = ['_id', '__v', 'title', 'description', 'price', 'images', 'createdAt'];
+            res.body.forEach(product => expect(product).to.have.all.keys(keys));
+            done();
+          });
+      });
+
+      after(async function() { await Product.deleteMany({}); });
     });
   });
 });
