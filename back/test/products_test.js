56ddaaeadcea03520b04d22e8a3730632b45fd03
Evgeny Morozov <evmorov@gmail.com>
2019-01-26 02:16:21 +0300

    Use async/await in tests

--- back/test/products_test.js
+++ back/test/products_test.js
@@ -13,14 +13,11 @@ const productKeys = ['id', 'title', 'description', 'price', 'images', 'createdAt
 describe('Products', function() {
   describe('#GET /products', function() {
     describe('no products', function() {
-      it('returns empty array', function(done) {
-        request(app).get('/products')
-          .end((err, res) => {
-            expect(res.statusCode).to.equal(200);
-            expect(res.body).to.be.an('array');
-            expect(res.body).to.be.empty;
-            done();
-          });
+      it('returns empty array', async function() {
+        const res = await request(app).get('/products');
+        expect(res.statusCode).to.equal(200);
+        expect(res.body).to.be.an('array');
+        expect(res.body).to.be.empty;
       });
     });
 
@@ -31,15 +28,12 @@ describe('Products', function() {
         await Product.create({ title: 'eee', description: 'fff', price: 3, images: [] });
       });
 
-      it('returns products', function(done) {
-        request(app).get('/products')
-          .end((err, res) => {
-            expect(res.statusCode).to.equal(200);
-            expect(res.body).to.be.an('array');
-            expect(res.body).to.have.lengthOf(3);
-            res.body.forEach(product => expect(product).to.have.all.keys(productKeys));
-            done();
-          });
+      it('returns products', async function() {
+        const res = await request(app).get('/products');
+        expect(res.statusCode).to.equal(200);
+        expect(res.body).to.be.an('array');
+        expect(res.body).to.have.lengthOf(3);
+        res.body.forEach(product => expect(product).to.have.all.keys(productKeys));
       });
 
       after(async function() { await Product.deleteMany({}); });
@@ -48,24 +42,18 @@ describe('Products', function() {
 
   describe('#GET /product/:id', function() {
     describe('not valid id', function() {
-      it('returns 400 and empty body', function(done) {
-        request(app).get('/products/1')
-          .end((err, res) => {
-            expect(res.statusCode).to.equal(400);
-            expect(res.body).to.deep.equal({});
-            done();
-          });
+      it('returns 400 and empty body', async function() {
+        const res = await request(app).get('/products/1');
+        expect(res.statusCode).to.equal(400);
+        expect(res.body).to.deep.equal({});
       });
     });
 
     describe('valid id but product is not found', function() {
-      it('returns 404 and empty body', function(done) {
-        request(app).get('/products/5c34fbfd608700dc5f5ef589')
-          .end((err, res) => {
-            expect(res.statusCode).to.equal(404);
-            expect(res.body).to.deep.equal({});
-            done();
-          });
+      it('returns 404 and empty body', async function() {
+        const res = await request(app).get('/products/5c34fbfd608700dc5f5ef589');
+        expect(res.statusCode).to.equal(404);
+        expect(res.body).to.deep.equal({});
       });
     });
 
@@ -76,18 +64,15 @@ describe('Products', function() {
         product = await Product.create({ title: 'aaa', description: 'bbb', price: 1, images: [] });
       });
 
-      it('returns the product', function(done) {
-        request(app).get(`/products/${product._id}`)
-          .end((err, res) => {
-            expect(res.statusCode).to.equal(200);
-            const actualProduct = res.body;
-            expect(actualProduct).to.have.all.keys(productKeys);
-            expect(actualProduct.title).to.equal(product.title);
-            expect(actualProduct.description).to.equal(product.description);
-            expect(actualProduct.price).to.equal(product.price);
-            expect(actualProduct.images).to.deep.equal(product.images);
-            done();
-          });
+      it('returns the product', async function() {
+        const res = await request(app).get(`/products/${product._id}`);
+        expect(res.statusCode).to.equal(200);
+        const actualProduct = res.body;
+        expect(actualProduct).to.have.all.keys(productKeys);
+        expect(actualProduct.title).to.equal(product.title);
+        expect(actualProduct.description).to.equal(product.description);
+        expect(actualProduct.price).to.equal(product.price);
+        expect(actualProduct.images).to.deep.equal(product.images);
       });
 
       after(async function() { await Product.deleteMany({}); });
@@ -96,24 +81,18 @@ describe('Products', function() {
 
   describe('#DELETE /product/:id', function() {
     describe('not valid id', function() {
-      it('returns 400 and empty body', function(done) {
-        request(app).delete('/products/1')
-          .end((err, res) => {
-            expect(res.statusCode).to.equal(400);
-            expect(res.body).to.deep.equal({});
-            done();
-          });
+      it('returns 400 and empty body', async function() {
+        const res = await request(app).delete('/products/1');
+        expect(res.statusCode).to.equal(400);
+        expect(res.body).to.deep.equal({});
       });
     });
 
     describe('valid id but product is not found', function() {
-      it('returns 404 and empty body', function(done) {
-        request(app).delete('/products/5c34fbfd608700dc5f5ef589')
-          .end((err, res) => {
-            expect(res.statusCode).to.equal(404);
-            expect(res.body).to.deep.equal({});
-            done();
-          });
+      it('returns 404 and empty body', async function() {
+        const res = await request(app).delete('/products/5c34fbfd608700dc5f5ef589');
+        expect(res.statusCode).to.equal(404);
+        expect(res.body).to.deep.equal({});
       });
     });
 
@@ -126,7 +105,6 @@ describe('Products', function() {
 
       it('destroys the product and returns it', async function() {
         const deleteRes = await request(app).delete(`/products/${product._id}`);
-
         expect(deleteRes.statusCode).to.equal(200);
         const actualProduct = deleteRes.body;
         expect(actualProduct).to.have.all.keys(productKeys);
@@ -136,7 +114,6 @@ describe('Products', function() {
         expect(actualProduct.images).to.deep.equal(product.images);
 
         const getRes = await request(app).get(`/products/${product._id}`);
-
         expect(getRes.statusCode).to.equal(404);
         expect(getRes.body).to.deep.equal({});
       });
