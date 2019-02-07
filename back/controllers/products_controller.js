88e27b88fb18de8eed753507cd656615d65026b7
Evgeny Morozov <evmorov@gmail.com>
2019-02-07 13:49:25 +0300

    Move middlewares to controller

--- back/controllers/products_controller.js
+++ back/controllers/products_controller.js
@@ -1,42 +1,74 @@
+const mongoose = require('mongoose');
+
 const Product = require('../models/product');
 
-exports.index = async (req, res) => {
-  const products = await Product.find();
-  res.json(products);
-};
+class ProductsController {
+  async fetchProductByReqId(req, res, next) {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      res.status(400).end();
+      return;
+    }
 
-exports.create = async (req, res) => {
-  const { title, description, price, images } = req.productParams;
-  let product = null;
-  try {
-    product = await Product.create({ title, description, price, images });
-  } catch (e) {
-    res.status(400).end(); // TODO: make a response with all validation errors
-    return;
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      req.product = product;
+      next();
+    } else {
+      res.status(404).end();
+    }
   }
-  res.status(201).json(product);
-};
 
-exports.show = async (req, res) => {
-  res.json(req.product);
-};
+  fetchProductParams(req, res, next) {
+    const productParams = req.body.product;
+    if (!productParams) {
+      res.status(400).end();
+      return;
+    }
 
-exports.update = async (req, res) => {
-  const { product } = req;
-  product.title = req.productParams.title;
-  product.description = req.productParams.description;
-  product.price = req.productParams.price;
-  product.images = req.productParams.images;
-  try {
-    await product.save();
-  } catch (e) {
-    res.status(400).end(); // TODO: make a response with all validation errors
-    return;
+    req.productParams = productParams;
+    next();
   }
-  res.json(product);
-};
 
-exports.destroy = async (req, res) => {
-  const product = await req.product.remove();
-  res.json(product);
-};
+  async index(req, res) {
+    const products = await Product.find();
+    res.json(products);
+  }
+
+  async create(req, res) {
+    const { title, description, price, images } = req.productParams;
+    let product = null;
+    try {
+      product = await Product.create({ title, description, price, images });
+    } catch (e) {
+      res.status(400).end(); // TODO: make a response with all validation errors
+      return;
+    }
+    res.status(201).json(product);
+  }
+
+  async show(req, res) {
+    res.json(req.product);
+  }
+
+  async update(req, res) {
+    const { product } = req;
+    product.title = req.productParams.title;
+    product.description = req.productParams.description;
+    product.price = req.productParams.price;
+    product.images = req.productParams.images;
+    try {
+      await product.save();
+    } catch (e) {
+      res.status(400).end(); // TODO: make a response with all validation errors
+      return;
+    }
+    res.json(product);
+  }
+
+  async destroy(req, res) {
+    const product = await req.product.remove();
+    res.json(product);
+  }
+}
+
+module.exports = ProductsController;
--- back/routes/products.js
+++ back/routes/products.js
@@ -1,44 +1,20 @@
 const express = require('express');
-const mongoose = require('mongoose');
 
-const Product = require('../models/product');
-const productsController = require('../controllers/products_controller');
+const ProductsController = require('../controllers/products_controller');
 
 const router = express.Router();
-
-const fetchProductByReqId = async (req, res, next) => {
-  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-    res.status(400).end();
-    return;
-  }
-
-  const product = await Product.findById(req.params.id);
-  if (product) {
-    req.product = product;
-    next();
-  } else {
-    res.status(404).end();
-  }
-};
-
-const fetchProductParams = (req, res, next) => {
-  const productParams = req.body.product;
-  if (!productParams) {
-    res.status(400).end();
-    return;
-  }
-
-  req.productParams = productParams;
-  next();
-};
+const productsController = new ProductsController();
+const {
+  fetchProductByReqId, fetchProductParams, index, show, create, update, destroy
+} = productsController;
 
 router.route('/')
-  .get(productsController.index)
-  .post(fetchProductParams, productsController.create);
+  .get(index)
+  .post(fetchProductParams, create);
 
 router.route('/:id')
-  .get(fetchProductByReqId, productsController.show)
-  .put([fetchProductByReqId, fetchProductParams], productsController.update)
-  .delete(fetchProductByReqId, productsController.destroy);
+  .get(fetchProductByReqId, show)
+  .put([fetchProductByReqId, fetchProductParams], update)
+  .delete(fetchProductByReqId, destroy);
 
 module.exports = router;
