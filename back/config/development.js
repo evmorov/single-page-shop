d9d38d838023381a9aeb52eae24557d327045055
Evgeny Morozov <evmorov@gmail.com>
2019-02-11 10:19:42 +0300

    Rename dbs

--- back/config/development.js
+++ back/config/development.js
@@ -1,5 +1,5 @@
 module.exports = {
   env: 'development',
-  db: 'mongodb://localhost:27017/express-react-shop',
+  db: 'mongodb://localhost:27017/single-page-shop',
   port: process.env.PORT || 5001,
 };
--- back/config/test.js
+++ back/config/test.js
@@ -1,5 +1,5 @@
 module.exports = {
   env: 'test',
-  db: 'mongodb://localhost:27017/express-react-shop-test',
+  db: 'mongodb://localhost:27017/single-page-shop-test',
   port: process.env.PORT || 5002,
 };
