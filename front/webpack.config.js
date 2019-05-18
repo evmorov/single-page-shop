0046e95385d987a5ab90ee047f2c6013f7dff1fc
Evgeny Morozov <evmorov@gmail.com>
2019-05-18 09:08:08 +0300

    Ability to define env variables

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,58 +1,63 @@
 const path = require('path');
+const webpack = require('webpack');
 
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
-const htmlPlugin = new HtmlWebPackPlugin({
-  template: './src/index.html',
-  filename: './index.html'
-});
+module.exports = env => {
+  const htmlPlugin = new HtmlWebPackPlugin({
+    template: './src/index.html',
+    filename: './index.html'
+  });
 
-module.exports = {
-  resolve: {
-    alias: {
-      '~': path.resolve(__dirname)
+  const environmentPlugin = new webpack.EnvironmentPlugin(env);
+
+  return {
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname)
+      },
+      extensions: ['*', '.js']
     },
-    extensions: ['*', '.js']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader'
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
           }
-        ]
-      },
-      {
-        test: /\.(ttf|eot|svg|woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: [{
-          loader: 'file-loader'
-        }]
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: [{
-          loader: 'file-loader'
-        }]
-      }
-    ]
-  },
-  devServer: {
-    hot: false,
-    inline: false,
-    historyApiFallback: true,
-    stats: 'minimal'
-  },
-  plugins: [htmlPlugin]
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: 'style-loader'
+            },
+            {
+              loader: 'css-loader'
+            }
+          ]
+        },
+        {
+          test: /\.(ttf|eot|svg|woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          use: [{
+            loader: 'file-loader'
+          }]
+        },
+        {
+          test: /\.(png|jpg|gif)$/,
+          use: [{
+            loader: 'file-loader'
+          }]
+        }
+      ]
+    },
+    devServer: {
+      hot: false,
+      inline: false,
+      historyApiFallback: true,
+      stats: 'minimal'
+    },
+    plugins: [htmlPlugin, environmentPlugin]
+  };
 };
